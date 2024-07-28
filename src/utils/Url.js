7a055d39f4434a92bd3421fc5727c4e7d4aa0417
Varunbingi <bingivarun27@gmail.com>
2024-07-28 11:39:39 +0530

    final commit

--- src/utils/Url.js
+++ src/utils/Url.js
@@ -1,7 +1,7 @@
 const DEFAULT_URL = "https://dummyjson.com/products?skip=0&limit=20";
 export default DEFAULT_URL;
 
-export const CATEGORIES_URL='https://dummyjson.com/products/categories';
+export const CATEGORIES_URL='https://dummyjson.com/products/category-list';
 export const SINGLE_CATEGORY_URL="https://dummyjson.com/products/category/";
 export const SEARCH_URL="https://dummyjson.com/products/search?q=";
 
